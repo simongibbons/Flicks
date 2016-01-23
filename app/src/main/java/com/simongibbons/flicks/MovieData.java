5bed8f4194d0f97aaf356498fe3753f1c7a485c4
Simon Gibbons <simongibbons@gmail.com>
2016-01-23 17:36:04 +0000

    Using okHTTP to grab movie data now to avoid creating hanging AsyncTasks.

--- app/src/main/java/com/simongibbons/flicks/MovieData.java
+++ app/src/main/java/com/simongibbons/flicks/MovieData.java
@@ -1,6 +1,37 @@
 package com.simongibbons.flicks;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MovieData {
     public String title = "";
     public String posterPath = "";
+
+    public static List<MovieData> getMovieDataFromJson(String moviesJsonStr) throws JSONException {
+        final String TMDB_RESULTS = "results";
+        final String TMDB_TITLE = "title";
+        final String TMDB_POSTER_PATH = "poster_path";
+
+        JSONObject moviesJson = new JSONObject(moviesJsonStr);
+        JSONArray moviesJsonArray = moviesJson.getJSONArray(TMDB_RESULTS);
+
+        List<MovieData> results = new ArrayList<>();
+
+        for (int i = 0; i < moviesJsonArray.length(); ++i) {
+            JSONObject movieJsonObject = moviesJsonArray.getJSONObject(i);
+
+            MovieData movie = new MovieData();
+
+            movie.title = movieJsonObject.getString(TMDB_TITLE);
+            movie.posterPath = movieJsonObject.getString(TMDB_POSTER_PATH);
+
+            results.add(movie);
+        }
+
+        return results;
+    }
 }
--- app/src/main/java/com/simongibbons/flicks/PosterListAdapter.java
+++ app/src/main/java/com/simongibbons/flicks/PosterListAdapter.java
@@ -2,10 +2,10 @@ package com.simongibbons.flicks;
 
 import android.content.Context;
 import android.net.Uri;
-import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,33 +13,31 @@ import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
 
-import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class PosterListAdapter extends RecyclerView.Adapter<PosterListAdapter.ViewHolder> {
 
     private Context mContext;
+    final OkHttpClient okHttpClient = new OkHttpClient();
+    final Handler handler = new Handler(Looper.getMainLooper());
     private List<MovieData> movieList = new ArrayList<>();
     private int nextPage = 1;
 
     public PosterListAdapter(Context context) {
         mContext = context;
 
-        PopularMoviesTask task = new PopularMoviesTask();
-        task.execute(nextPage);
+        loadMoreMovies();
     }
-
-
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -61,8 +59,7 @@ public class PosterListAdapter extends RecyclerView.Adapter<PosterListAdapter.Vi
 
         // Check if we need to grab more data
         if(position == (getItemCount() - 1) ){
-            PopularMoviesTask task = new PopularMoviesTask();
-            task.execute(nextPage);
+            loadMoreMovies();
         }
     }
 
@@ -83,122 +80,53 @@ public class PosterListAdapter extends RecyclerView.Adapter<PosterListAdapter.Vi
         }
     }
 
-    public class PopularMoviesTask extends AsyncTask<Integer, Void, List<MovieData> > {
 
-        final String LOG_TAG = PopularMoviesTask.class.getSimpleName();
+    private void loadMoreMovies() {
+        final String BASE_URL =
+                "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc";
+        final String PAGE_PARAM = "page";
+        final String API_KEY_PARAM = "api_key";
+
+        Uri builtUri = Uri.parse(BASE_URL).buildUpon()
+                .appendQueryParameter(PAGE_PARAM, Integer.toString(nextPage))
+                .appendQueryParameter(API_KEY_PARAM, BuildConfig.THE_MOVIE_DB_API_KEY)
+                .build();
+
+        Request request = new Request.Builder()
+                .url(builtUri.toString())
+                .build();
+
+        okHttpClient.newCall(request).enqueue(new LoadMovieCallback());
+    }
+
+    private class LoadMovieCallback implements Callback {
+        @Override
+        public void onFailure(Call call, IOException e) {
+            e.printStackTrace();
+        }
 
         @Override
-        protected List<MovieData> doInBackground(Integer... params) {
-            // These two need to be declared outside the try/catch
-            // so that they can be closed in the finally block.
-            HttpURLConnection urlConnection = null;
-            BufferedReader reader = null;
-
-            // Will contain the raw JSON response as a string.
-            String moviesJsonStr = null;
-
-
+        public void onResponse(Call call, Response response) throws IOException {
+            List<MovieData> movies;
             try {
-                final String BASE_URL =
-                        "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc";
-                final String PAGE_PARAM = "page";
-                final String API_KEY_PARAM = "api_key";
-
-                Uri builtUri = Uri.parse(BASE_URL).buildUpon()
-                        .appendQueryParameter(PAGE_PARAM, params[0].toString())
-                        .appendQueryParameter(API_KEY_PARAM, BuildConfig.THE_MOVIE_DB_API_KEY)
-                        .build();
-
-                URL url = new URL(builtUri.toString());
-
-                // Create the request to OpenWeatherMap, and open the connection
-                urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setRequestMethod("GET");
-                urlConnection.connect();
-
-                // Read the input stream into a String
-                InputStream inputStream = urlConnection.getInputStream();
-                StringBuilder buffer = new StringBuilder();
-                if (inputStream == null) {
-                    // Nothing to do.
-                    return null;
-                }
-                reader = new BufferedReader(new InputStreamReader(inputStream));
-
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    buffer.append(line);
-                }
-
-                if (buffer.length() == 0) {
-                    // Stream was empty.  No point in parsing.
-                    return null;
-                }
-                moviesJsonStr = buffer.toString();
-            } catch (IOException e) {
-                Log.e(LOG_TAG, "Error ", e);
-                return null;
-            } finally {
-                if (urlConnection != null) {
-                    urlConnection.disconnect();
-                }
-                if (reader != null) {
-                    try {
-                        reader.close();
-                    } catch (final IOException e) {
-                        Log.e(LOG_TAG, "Error closing stream", e);
-                    }
-                }
-            }
-
-            try {
-                return getMovieDataFromJson(moviesJsonStr);
+                movies = MovieData.getMovieDataFromJson(response.body().string());
             } catch (JSONException e) {
-                Log.e(LOG_TAG, e.getMessage(), e);
                 e.printStackTrace();
+                return;
             }
 
-            // This will only happen if there was an error getting or parsing the forecast.
-            return null;
-        }
-
-        private List<MovieData> getMovieDataFromJson(String moviesJsonStr) throws JSONException {
-
-            final String TMDB_RESULTS = "results";
-            final String TMDB_TITLE = "title";
-            final String TMDB_POSTER_PATH = "poster_path";
-
-            JSONObject moviesJson = new JSONObject(moviesJsonStr);
-            JSONArray moviesJsonArray = moviesJson.getJSONArray(TMDB_RESULTS);
-
-            List<MovieData> results = new ArrayList<>();
-
-            for(int i = 0 ; i < moviesJsonArray.length() ; ++i) {
-                JSONObject movieJsonObject = moviesJsonArray.getJSONObject(i);
-
-                MovieData movie = new MovieData();
-
-                movie.title = movieJsonObject.getString(TMDB_TITLE);
-                movie.posterPath = movieJsonObject.getString(TMDB_POSTER_PATH);
-
-                results.add(movie);
+            for(MovieData movie : movies) {
+                movieList.add(movie);
             }
 
-            return results;
-        }
+            nextPage += 1;
 
-        @Override
-        protected void onPostExecute(List<MovieData> result) {
-            super.onPostExecute(result);
-
-            if(result != null) {
-                for (MovieData movie : result) {
-                    movieList.add(movie);
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    notifyDataSetChanged();
                 }
-                nextPage += 1;
-            }
-
-            notifyDataSetChanged();
+            });
         }
     }
 

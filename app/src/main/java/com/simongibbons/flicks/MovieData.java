019d720597708fb8a281a5a1fdb0de2d9ee7be57
Simon Gibbons <simongibbons@gmail.com>
2016-01-23 18:19:09 +0000

    Impliment Parcellable interface for MovieData

--- app/src/main/java/com/simongibbons/flicks/MovieData.java
+++ app/src/main/java/com/simongibbons/flicks/MovieData.java
@@ -1,9 +1,12 @@
 package com.simongibbons.flicks;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class MovieData {
+public class MovieData implements Parcelable {
     public String title;
     public String posterPath;
 
@@ -19,4 +22,31 @@ public class MovieData {
 
         return movie;
     }
+
+    // Methods for Parcelable implementation
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(title);
+        dest.writeString(posterPath);
+    }
+
+    public static final Parcelable.Creator<MovieData> CREATOR
+            = new Parcelable.Creator<MovieData>() {
+        public MovieData createFromParcel(Parcel in) {
+            MovieData movieData = new MovieData();
+            movieData.title = in.readString();
+            movieData.posterPath = in.readString();
+
+            return movieData;
+        }
+
+        public MovieData[] newArray(int size) {
+            return new MovieData[size];
+        }
+    };
 }

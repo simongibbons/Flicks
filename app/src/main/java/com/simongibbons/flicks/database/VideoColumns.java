870c2284af96e7de2ca85dd5a1a0db82ddedc20a
Simon Gibbons <simongibbons@gmail.com>
2016-01-31 23:45:43 +0000

    Added videoid to database schema

--- app/src/main/java/com/simongibbons/flicks/database/VideoColumns.java
+++ app/src/main/java/com/simongibbons/flicks/database/VideoColumns.java
@@ -1,15 +1,21 @@
 package com.simongibbons.flicks.database;
 
 import net.simonvt.schematic.annotation.AutoIncrement;
+import net.simonvt.schematic.annotation.ConflictResolutionType;
 import net.simonvt.schematic.annotation.DataType;
 import net.simonvt.schematic.annotation.PrimaryKey;
 import net.simonvt.schematic.annotation.References;
+import net.simonvt.schematic.annotation.Unique;
 
 
 public class VideoColumns {
     @DataType(DataType.Type.INTEGER) @PrimaryKey
     @AutoIncrement
     public static final String _ID = "_id";
+
+    @DataType(DataType.Type.TEXT)
+    @Unique(onConflict = ConflictResolutionType.REPLACE)
+    public static final String VIDEOID = "videoid";
 
     @DataType(DataType.Type.INTEGER)
     @References(table = MovieDatabase.MOVIES, column = MovieColumns.MOVIEID)
